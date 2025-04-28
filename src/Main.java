b90b40955669993e597814d7fa918d473ab061fb
VladimirVorobyev <vladimirv15012010@gmail.com>
2025-04-28 14:32:20 +0300

    add principal input and foolproof for it

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,43 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        double principal;
+        double rate;
+        int timesCompounded;
+        int years;
+        double amount;
+
+        System.out.print("Enter the principal amount: ");
+        principal = scanner.nextDouble();
+
+        if (principal < 0){
+            System.out.println("Invalid date! ");
+            principal = scanner.nextDouble();
+        }
+        else if(principal == 0){
+            System.out.println("it's pointless with 0.");
+            int choise;
+            System.out.println("Select your action :\n 1) continue\n 2) stop ");
+            choise = scanner.nextInt();
+            if (choise < 1 || choise > 2){
+                System.out.println("Error!");
+                return;
+            }
+            else if (choise == 2){
+                return;
+            }
+            else {
+                System.out.println("Enter principal amount: ");
+                principal = scanner.nextDouble();
+            }
+            }
+
+        System.out.print("Enter rate (in %): ");
+        rate = scanner.nextDouble();
 
 
+        System.out.print(principal);
+
+        scanner.close();
     }
 }
